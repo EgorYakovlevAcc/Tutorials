7cc4ac194ee083604e18c25d944348413e18d68b
KateKlyukina <katya.klyukina.1999@mail.ru>
2019-10-04 01:33:05 +0300

    List fish

--- src/ru/tech/Main.java
+++ src/ru/tech/Main.java
@@ -1,5 +1,10 @@
 package ru.tech;
 
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,5 +13,15 @@ public class Main {
         dog.getGavGav();
         Fish fish = new Fish();
         fish.getBulBul();
+        System.out.println("here is a List of fishes " + getFishList().toString());
+    }
+
+    public static List<Fish> getFishList(){
+        List<Fish> fishList = new ArrayList<Fish>();
+        for (int i = 0; i < 5; i++){
+            Fish fish=new Fish();
+            fishList.add(fish);
+        }
+        return fishList;
     }
 }
