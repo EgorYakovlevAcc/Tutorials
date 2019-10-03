723e7c1ef3dda16b35b1a00b5dbcbd8d2508726c
Egor Yakovlev <yakovlev.es@phystech.edu>
2019-10-04 01:41:05 +0300

    fish props

--- src/ru/tech/Main.java
+++ src/ru/tech/Main.java
@@ -4,6 +4,7 @@ import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 
@@ -13,15 +14,29 @@ public class Main {
         dog.getGavGav();
         Fish fish = new Fish();
         fish.getBulBul();
-        System.out.println("here is a List of fishes " + getFishList().toString());
+        for (Fish fish1: getFishList()) {
+            System.out.println(fish1);
+            System.out.println(fish1.high);
+            System.out.println(fish1.speed);
+            System.out.println(fish1.weigth);
+        }
     }
 
     public static List<Fish> getFishList(){
         List<Fish> fishList = new ArrayList<Fish>();
         for (int i = 0; i < 5; i++){
-            Fish fish=new Fish();
+            Fish fish = new Fish();
+            setFishProperties(fish);
             fishList.add(fish);
         }
         return fishList;
+    }
+
+    public static void setFishProperties(Fish fish) {
+        Random random = new Random();
+
+        fish.speed = random.nextInt();
+        fish.high = random.nextInt();
+        fish.weigth = random.nextInt();
     }
 }
